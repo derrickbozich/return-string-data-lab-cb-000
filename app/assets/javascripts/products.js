903731c48e6057e8d34bbc933740de2d04207684
derrick b <dbozich1@gmail.com>
2018-10-16 19:45:21 +0000

    automatically backed up by learn

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -3,10 +3,17 @@ $(function(){
     const id = parseInt(this.dataset.id)
     const description = `product/${id}/description`
     const inventory = `product/${id}/inventory`
+    
     $.get(description, data => {
       $('#product-' + id).text(data)
     })
 
+    $.get(inventory, data => {
+      $('#product-' + id).text(data)
+    })
+
     
+
+
   })
 })
