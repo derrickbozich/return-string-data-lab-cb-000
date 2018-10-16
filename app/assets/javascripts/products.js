595287b56cf7bad029f94f6032880fb8a7656e45
derrick b <dbozich1@gmail.com>
2018-10-16 19:43:36 +0000

    automatically backed up by learn

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,6 +1,5 @@
 $(function(){
   $('.js-more').on('click', function(){
-    console.log('stop')
     const id = parseInt(this.dataset.id)
     const url = `product/${id}/description`
     $.get(url, data => {
