889f7861f6a2b37f8f8b89355eb9796faecb5a6c
derrick b <dbozich1@gmail.com>
2018-10-16 19:48:44 +0000

    automatically backed up by learn

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -3,16 +3,18 @@ $(function(){
     const id = parseInt(this.dataset.id)
     const description = `product/${id}/description`
     const inventory = `product/${id}/inventory`
-    
+
     $.get(description, data => {
+      let pTag = string.concat()
       $('#product-' + id).text(data)
     })
 
     $.get(inventory, data => {
+      debugger
       $('#product-' + id).text(data)
     })
 
-    
+
 
 
   })
