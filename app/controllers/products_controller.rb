efda5765c08e1cb14eb3ffd82ba48cb74e1ef417
derrick b <dbozich1@gmail.com>
2018-10-16 19:05:04 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,5 +2,14 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def description
+    product = Product.find_by_id(params['id'])
+    if product
+      render :plain product.description
+    else
+      
+    end
+  end
   
 end
